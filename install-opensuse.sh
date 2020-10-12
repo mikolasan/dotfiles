45e73900cec3bc4de1c00df0fa72d64b4f606078
Nikolay Neupokoev <mikolasan@users.noreply.github.com>
2020-10-12 02:29:43 -0700

    Add mosh (opensuse)

--- install-opensuse.sh
+++ install-opensuse.sh
@@ -18,4 +18,14 @@ pip3 install --user ranger-fm powerline-status thefuck
 vim +PluginInstall +qall
 ./fzf/install # n, y, y
 chsh --shell /bin/zsh $USER
+
+# Mosh
+sudo zypper install git make autoconf automake gcc pkg-config libopenssl-devel zlib-devel protobuf-c protobuf-devel libncurses5 ncurses5-devel
+git clone https://github.com/mobile-shell/mosh
+cd mosh
+./autogen.sh
+./configure
+make
+sudo make install
+
 echo "It is time to LOG OUT and then start fresh"
