4ef101f4fcd4b4ff25eed0e10fbac456d59bf2cf
Nikolay Neupokoev <nikolay@neupokoev.xyz>
2025-07-29 12:29:56 -0700

    vim: add some plugins and Rust

--- lvim/config.lua
+++ lvim/config.lua
@@ -14,10 +14,13 @@ vim.cmd([[
   set ignorecase smartcase  " ignore case only when the pattern contains no capital letters
 ]])
 
+lvim.keys.normal_mode["<leader>y"] = '"+y'
 lvim.keys.normal_mode["|"] = ":vsplit<CR>"
 lvim.keys.normal_mode["-"] = ":split<CR>"
 
 lvim.keys.normal_mode["<CS-f>"] = ":GrugFar<CR>"
+vim.keymap.set('n', "<C-f>", function() require('grug-far').open({ prefills = { paths = vim.fn.expand("%") } }) end)
+vim.keymap.set('n', "<leader><tab>f", function() require('grug-far').open({ prefills = { search = vim.fn.expand("<cword>") } }) end)
 
 lvim.builtin.which_key.mappings["t"] = {
   name = "+Terminal",
@@ -26,13 +29,23 @@ lvim.builtin.which_key.mappings["t"] = {
   h = { "<cmd>2ToggleTerm size=30 direction=horizontal<cr>", "Split horizontal" },
 }
 
+lvim.builtin.which_key.mappings["T"] = {
+  name = "+Tasks (overseer)",
+  t = { "<cmd>OverseerToggle<CR>", "Toggle" },
+  r = { "<cmd>OverseerRun<CR>", "Run" },
+}
+
 vim.keymap.set('n', '<F5>', function() require('dap').continue() end)
 vim.keymap.set('n', '<F10>', function() require('dap').step_over() end)
 vim.keymap.set('n', '<F11>', function() require('dap').step_into() end)
 vim.keymap.set('n', '<F12>', function() require('dap').step_out() end)
 
+-- bufferline
 vim.keymap.set("n", "<C-h>", ":BufferLineCyclePrev<CR>")
 vim.keymap.set("n", "<C-l>", ":BufferLineCycleNext<CR>")
+vim.keymap.set("n", "<leader><tab>d", ":bd<CR>")
+vim.keymap.set("n", "<leader><tab>p", ":BufferLinePick<CR>")
+
 -- GO
 
 ------------------------
@@ -44,7 +57,14 @@ lvim.builtin.treesitter.ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 
 -- Plugins
 ------------------------
 lvim.plugins = {
-  "brooth/far.vim",
+  {
+    'stevearc/overseer.nvim',
+    opts = {},
+  },
+  "nvim-neotest/nvim-nio",
+  -- "brooth/far.vim",
+  "tpope/vim-fugitive",
+  { "tpope/vim-repeat" },
   { 'MagicDuck/grug-far.nvim',
     version = "1.6.3",
     config = function()
@@ -58,9 +78,11 @@ lvim.plugins = {
   "mg979/vim-visual-multi",
 --  "olexsmir/gopher.nvim",
 
+  "hashivim/vim-terraform",
   "leoluz/nvim-dap-go",
   {
     "kdheepak/lazygit.nvim",
+    commit = "4839ab642962cc76bb1bf278427dc4c59be15072",
     lazy = true,
     cmd = {
         "LazyGit",
@@ -79,9 +101,28 @@ lvim.plugins = {
         { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
     }
   },
+  "simrat39/rust-tools.nvim",
   {
-    'stevearc/overseer.nvim',
-    opts = {},
+    "saecki/crates.nvim",
+    version = "v0.3.0",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("crates").setup {
+        null_ls = {
+          enabled = true,
+          name = "crates.nvim",
+        },
+        popup = {
+          border = "rounded",
+        },
+      }
+    end,
+  },
+  {
+    "j-hui/fidget.nvim",
+    config = function()
+      require("fidget").setup()
+    end,
   },
 }
 
@@ -117,6 +158,7 @@ dapgo.setup()
 -- LSP
 ------------------------
 vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "gopls" })
+vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "rust_analyzer" })
 
 local lsp_manager = require "lvim.lsp.manager"
 -- lsp_manager.setup("golangci_lint_ls", {
@@ -176,3 +218,120 @@ lsp_manager.setup("gopls", {
 --   },
 -- }
 -- 
+-----------
+-- Rust
+-----------
+
+local mason_path = vim.fn.glob(vim.fn.stdpath "data" .. "/mason/")
+
+local codelldb_path = mason_path .. "bin/codelldb"
+local liblldb_path = mason_path .. "packages/codelldb/extension/lldb/lib/liblldb"
+local this_os = vim.loop.os_uname().sysname
+
+-- The path in windows is different
+if this_os:find "Windows" then
+  codelldb_path = mason_path .. "packages\\codelldb\\extension\\adapter\\codelldb.exe"
+  liblldb_path = mason_path .. "packages\\codelldb\\extension\\lldb\\bin\\liblldb.dll"
+else
+  -- The liblldb extension is .so for linux and .dylib for macOS
+  liblldb_path = liblldb_path .. (this_os == "Linux" and ".so" or ".dylib")
+end
+
+pcall(function()
+  require("rust-tools").setup {
+    tools = {
+      executor = require("rust-tools/executors").termopen, -- can be quickfix or termopen
+      reload_workspace_from_cargo_toml = true,
+      runnables = {
+        use_telescope = true,
+      },
+      inlay_hints = {
+        auto = true,
+        only_current_line = false,
+        show_parameter_hints = false,
+        parameter_hints_prefix = "<-",
+        other_hints_prefix = "=>",
+        max_len_align = false,
+        max_len_align_padding = 1,
+        right_align = false,
+        right_align_padding = 7,
+        highlight = "Comment",
+      },
+      hover_actions = {
+        border = "rounded",
+      },
+      on_initialized = function()
+        vim.api.nvim_create_autocmd({ "BufWritePost", "BufEnter", "CursorHold", "InsertLeave" }, {
+          pattern = { "*.rs" },
+          callback = function()
+            local _, _ = pcall(vim.lsp.codelens.refresh)
+          end,
+        })
+      end,
+    },
+    dap = {
+      -- adapter= codelldb_adapter,
+      adapter = require("rust-tools.dap").get_codelldb_adapter(codelldb_path, liblldb_path),
+    },
+    server = {
+      on_attach = function(client, bufnr)
+        require("lvim.lsp").common_on_attach(client, bufnr)
+        local rt = require "rust-tools"
+        vim.keymap.set("n", "K", rt.hover_actions.hover_actions, { buffer = bufnr })
+      end,
+
+      capabilities = require("lvim.lsp").common_capabilities(),
+      settings = {
+        ["rust-analyzer"] = {
+          lens = {
+            enable = true,
+          },
+          checkOnSave = {
+            enable = true,
+            command = "clippy",
+          },
+        },
+      },
+    },
+  }
+end)
+
+lvim.builtin.dap.on_config_done = function(dap)
+  dap.adapters.codelldb = require("rust-tools.dap").get_codelldb_adapter(codelldb_path, liblldb_path)
+  dap.configurations.rust = {
+    {
+      name = "Launch file",
+      type = "codelldb",
+      request = "launch",
+      program = function()
+        return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+      end,
+      cwd = "${workspaceFolder}",
+      stopOnEntry = false,
+    },
+  }
+end
+
+vim.api.nvim_set_keymap("n", "<m-d>", "<cmd>RustOpenExternalDocs<Cr>", { noremap = true, silent = true })
+
+lvim.builtin.which_key.mappings["C"] = {
+  name = "Rust",
+  r = { "<cmd>RustRunnables<Cr>", "Runnables" },
+  t = { "<cmd>lua _CARGO_TEST()<cr>", "Cargo Test" },
+  m = { "<cmd>RustExpandMacro<Cr>", "Expand Macro" },
+  c = { "<cmd>RustOpenCargo<Cr>", "Open Cargo" },
+  p = { "<cmd>RustParentModule<Cr>", "Parent Module" },
+  d = { "<cmd>RustDebuggables<Cr>", "Debuggables" },
+  v = { "<cmd>RustViewCrateGraph<Cr>", "View Crate Graph" },
+  R = {
+    "<cmd>lua require('rust-tools/workspace_refresh')._reload_workspace_from_cargo_toml()<Cr>",
+    "Reload Workspace",
+  },
+  o = { "<cmd>RustOpenExternalDocs<Cr>", "Open External Docs" },
+  y = { "<cmd>lua require'crates'.open_repository()<cr>", "[crates] open repository" },
+  P = { "<cmd>lua require'crates'.show_popup()<cr>", "[crates] show popup" },
+  i = { "<cmd>lua require'crates'.show_crate_popup()<cr>", "[crates] show info" },
+  f = { "<cmd>lua require'crates'.show_features_popup()<cr>", "[crates] show features" },
+  D = { "<cmd>lua require'crates'.show_dependencies_popup()<cr>", "[crates] show dependencies" },
+}
+
